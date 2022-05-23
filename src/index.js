dc83f0bd2e338c13ae06390c6c3f73aa7d045981
Judy Palacios <palacios.carolina0@gmail.com>
2022-05-22 19:30:59 -0500

    Eliminé membership.html, función validator.isvalid terminada y avance CSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+document.getElementById("enter").addEventListener('click', function() {
+    document.getElementById("hidden");
+});
+
+
+document.getElementById("valid").addEventListener('click',function() {
+    let creditcardNumber = document.getElementById("cardNumber").value;
+
+    let luhnNumber = validator.isValid (creditcardNumber);
+
+    if (luhnNumber === true) {
+        document.getElementById("theValidation").innerHTML = "¡Felicidades! número de tarjeta válido";
+    } else {
+        document.getElementById("theValidation").innerHTML = "Lo siento, número de tarjeta inválido";
+    }
+
+})
+
+
+
+validator.maskify (creditcardNumber);
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,47 @@
 const validator = {
-  // ...
+
+  isValid: function (creditcardNumber) {
+    //recibo num tarjeta crédito y lo vuelo array, luego cada dígito a número y lo reverseo
+    let arR = creditcardNumber.split('').map(function(x) {return parseInt(x)}).reverse();
+
+    for (let i = 1; i < arR.length; i+=2) {  //recorrer array desde i posición 1 y luego intercalado
+      let number = arR[i] * 2;               // el número con índice i lo multiplico por 2
+      let sum = 0;
+
+      if(number >= 10){
+              
+        while (number > 0) {
+        sum += number % 10;   //agregar a variable suma, el residuo en cada iteración y así reducir los números mayores a 10
+        number = Math.floor(number / 10);   //redondear(entero) hacia abajo el número de la división
+        }
+      } 
+      if(sum > 0) number = sum;
+      arR[i] = number;   //número resultante y reducido (posición impar)
+    }
+  
+    const resTotal = arR.reduce(function(a,b){ 
+    return a+b;
+    })
+  
+    if (resTotal%10 === 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  
+  
 };
+  
+  
+
+ 
+
+
+   
+
+
+
 
 export default validator;
