8471cf0494b1fff67855c46702a126c0b0a7d4fe
Judy Palacios <palacios.carolina0@gmail.com>
2022-05-26 23:08:21 -0500

    Nuevo diseño de interfaz,nuevo css, validator y maskify pasaron test

--- src/index.js
+++ src/index.js
@@ -2,27 +2,39 @@ import validator from './validator.js';
 
 console.log(validator);
 
-document.getElementById("enter").addEventListener('click', function() {
-    document.getElementById("hidden");
+document.getElementById("enter").addEventListener('click',()=> {
+    document.getElementById("hidd").classList.remove("hidden");
+    document.getElementById("firstPage").style.display="none";
 });
 
+let creditcardNumber = document.getElementById("cardNumber");
 
-document.getElementById("valid").addEventListener('click',function() {
-    let creditcardNumber = document.getElementById("cardNumber").value;
-
-    let luhnNumber = validator.isValid (creditcardNumber);
-
-    if (luhnNumber === true) {
+function validation(){
+    let luhnNumber = validator.isValid(creditcardNumber.value);
+    creditcardNumber.value = validator.maskify(creditcardNumber.value);
+      if (luhnNumber === true) {
         document.getElementById("theValidation").innerHTML = "¡Felicidades! número de tarjeta válido";
-    } else {
-        document.getElementById("theValidation").innerHTML = "Lo siento, número de tarjeta inválido";
+        document.getElementById("theValidation").style.display='block';
     }
+      else {
+        document.getElementById("theValidation").innerHTML = "Lo siento, número de tarjeta inválido";
+        document.getElementById("theValidation").style.display='block';
+    }
+}
 
-})
+document.getElementById("valid").addEventListener('click',validation); //escuchar evento para función validator
+
+function removeDiv() {
+    document.getElementById("theValidation").style.display='none';
+}
+document.getElementById("clean").addEventListener('click',removeDiv);
 
 
 
-validator.maskify (creditcardNumber);
+
+
+
+
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -8,14 +8,13 @@ const validator = {
       let number = arR[i] * 2;               // el número con índice i lo multiplico por 2
       let sum = 0;
 
-      if(number >= 10){
-              
+      if(number >= 10){   
         while (number > 0) {
         sum += number % 10;   //agregar a variable suma, el residuo en cada iteración y así reducir los números mayores a 10
         number = Math.floor(number / 10);   //redondear(entero) hacia abajo el número de la división
         }
       } 
-      if(sum > 0) number = sum;
+      if(sum > 0) number = sum
       arR[i] = number;   //número resultante y reducido (posición impar)
     }
   
@@ -28,20 +27,24 @@ const validator = {
     } else {
       return false;
     }
-  }
+  },
 
-  
+  maskify: function (creditcardNumber) {
+    
+    const size = creditcardNumber.length - 5;  //total de caracteres a encriptar
+    let maskString = "";
+
+    for (let i=0; i < creditcardNumber.length; i++) { 
+      if (i <= size){      //iterar deacuerdo a los índices menores al "total de caracteres a encriptar"
+        maskString = maskString + "#";   //sustituir por # y agregar a la variable en cada iteración
+      } else {
+        maskString = maskString + creditcardNumber[i]; //si no cumple agregar el número o letra
+      }
+    }
+   return maskString;
+  }
   
 };
   
-  
-
- 
-
-
-   
-
-
-
 
 export default validator;
